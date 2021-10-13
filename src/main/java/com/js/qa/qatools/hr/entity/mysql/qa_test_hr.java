349bd65cf84db36f010122639ddd7b1e608a8cac
손정빈 <wjdqls62@naver.com>
2021-10-13 22:25:35 +0900

    #22 - 변수명을 카멜표기법으로 변경(JPA 제약사항)

--- src/main/java/com/js/qa/qatools/hr/entity/mysql/qa_test_hr.java
+++ src/main/java/com/js/qa/qatools/hr/entity/mysql/qa_test_hr.java
@@ -22,61 +22,62 @@ public class qa_test_hr extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer NO;
+    @Column(name = "NO")
+    private Integer no;
 
-    @Column(length = 30)
-    private String NAME;
+    @Column(length = 30, name = "NAME")
+    private String name;
 
-    @Column(length = 255)
-    private String GRP;
+    @Column(length = 255, name = "GRP")
+    private String grp;
 
-    @Column(length = 1)
-    private Character GRP_ADMIN;
+    @Column(length = 1, name = "GRP_ADMIN")
+    private Character grpAdmin;
 
-    @Column(length = 255)
-    private String DOMAIN;
+    @Column(length = 255, name = "DOMAIN")
+    private String domain;
 
-    @Column(length = 1)
-    private String EARS_USE;
+    @Column(length = 1, name = "EARS_USE")
+    private Character earsUse;
 
-    @Column(length = 1)
-    private Character EARS_EXPIRE_ACTION;
+    @Column(length = 1, name = "EARS_EXPIRE_ACTION")
+    private Character earsExpireAction;
 
-    @Column(length = 100)
-    private String ROUTE_SERVER;
+    @Column(length = 100, name = "ROUTE_SERVER")
+    private String routeServer;
 
-    @Column(length = 100)
-    private String FORWARD_SERVER;
+    @Column(length = 100, name = "FORWARD_SERVER")
+    private String forwardServer;
 
-    @Column(length = 255)
-    private String ROUTE_EMAIL;
+    @Column(length = 255, name = "ROUTE_EMAIL")
+    private String routeEmail;
 
-    @Column(length = 1)
-    private Character RUN_MODE;
+    @Column(length = 1, name = "RUN_MODE")
+    private Character runMode;
 
-    @Column(length = 8)
-    private String FILTER_ORDER;
+    @Column(length = 8, name = "FILTER_ORDER")
+    private String filterOrder;
 
-    @Column(length = 1)
-    private Character APT_USE;
+    @Column(length = 1, name = "APT_USE")
+    private Character aptUse;
 
-    @Column(length = 1)
-    private Character DETOX_USE;
+    @Column(length = 1, name = "DETOX_USE")
+    private Character detoxUse;
 
-    @Column(length = 2)
-    private String LANG;
+    @Column(length = 2, name = "LANG")
+    private String lang;
 
     @ManyToOne
     @JoinColumn(name = "USR_GRADE")
-    private user_grade USR_GRADE;
+    private user_grade usrGrade;
 
     @ManyToOne
     @JoinColumn(name = "USR_DEPT")
-    private dept_info DEPT_INFO;
+    private dept_info usrDept;
 
-    @Column(length = 30)
-    private String USR_MANAGER;
+    @Column(length = 30, name = "USR_MANAGER")
+    private String usrManager;
 
-    @Column(length = 1)
-    private String IS_VALID;
+    @Column(length = 1, name = "IS_VALID")
+    private String isValid;
 }
