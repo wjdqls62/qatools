4544e27c108c0b7dcc5a43d470ecd14f1a4e62e4
손정빈 <wjdqls62@naver.com>
2021-10-20 14:07:46 +0900

    #24 - Entity 클래스 추가   - 기본 Schema가 'dbo' 가 아니므로 @Table (schema="insa") 추가

---
+++ src/main/java/com/js/qa/qatools/hr/entity/sqlserver/dept_info.java
@@ -0,0 +1,31 @@
+package com.js.qa.qatools.hr.entity.sqlserver;
+
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Table (schema = "insa")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class dept_info {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int dept_num;
+
+    @Column(length = 50)
+    private String dept_name;
+
+    @Column(length = 50)
+    private String dept_parent;
+
+    public void changeDeptName(String dept_name){
+        this.dept_name = dept_name;
+    }
+
+    public void changeParent(String dept_parent){
+        this.dept_parent = dept_parent;
+    }
+}
---
+++ src/main/java/com/js/qa/qatools/hr/entity/sqlserver/qa_test_hr.java
@@ -0,0 +1,82 @@
+package com.js.qa.qatools.hr.entity.sqlserver;
+
+import com.js.qa.qatools.hr.entity.BaseEntity;
+import lombok.*;
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table (schema = "insa")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Builder
+public class qa_test_hr  {
+
+    @Column(length = 255)
+    private String EMAIL;
+
+    @Column(length = 64)
+    private String PASSWORD;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "NO")
+    private Integer no;
+
+    @Column(length = 30, name = "NAME")
+    private String name;
+
+    @Column(length = 255, name = "GRP")
+    private String grp;
+
+    @Column(length = 1, name = "GRP_ADMIN")
+    private Character grpAdmin;
+
+    @Column(length = 255, name = "DOMAIN")
+    private String domain;
+
+    @Column(length = 1, name = "EARS_USE")
+    private Character earsUse;
+
+    @Column(length = 1, name = "EARS_EXPIRE_ACTION")
+    private Character earsExpireAction;
+
+    @Column(length = 100, name = "ROUTE_SERVER")
+    private String routeServer;
+
+    @Column(length = 100, name = "FORWARD_SERVER")
+    private String forwardServer;
+
+    @Column(length = 255, name = "ROUTE_EMAIL")
+    private String routeEmail;
+
+    @Column(length = 1, name = "RUN_MODE")
+    private Character runMode;
+
+    @Column(length = 8, name = "FILTER_ORDER")
+    private String filterOrder;
+
+    @Column(length = 1, name = "APT_USE")
+    private Character aptUse;
+
+    @Column(length = 1, name = "DETOX_USE")
+    private Character detoxUse;
+
+    @Column(length = 2, name = "LANG")
+    private String lang;
+
+    @ManyToOne
+    @JoinColumn(name = "USR_GRADE")
+    private user_grade usrGrade;
+
+    @ManyToOne
+    @JoinColumn(name = "USR_DEPT")
+    private dept_info usrDept;
+
+    @Column(length = 30, name = "USR_MANAGER")
+    private String usrManager;
+
+    @Column(length = 1, name = "IS_VALID")
+    private String isValid;
+}
