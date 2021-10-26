4316c69157cf74daf07350f6d2143c52ec29ecb8
손정빈 <wjdqls62@naver.com>
2021-10-26 21:57:22 +0900

    #24 - MySQL Repository 인터페이스를 SQL Server로 추가

--- src/main/java/com/js/qa/qatools/hr/repository/sqlserver/SqlServer_DeptInfo.java
+++ src/main/java/com/js/qa/qatools/hr/repository/sqlserver/SqlServer_DeptInfo.java
@@ -1,8 +1,8 @@
 package com.js.qa.qatools.hr.repository.sqlserver;
 
-import com.js.qa.qatools.hr.entity.sqlserver.dept_info;
+import com.js.qa.qatools.hr.entity.sqlserver.sqlserver_dept_info;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface SqlServer_DeptInfo extends JpaRepository<dept_info, Integer>, QuerydslPredicateExecutor<dept_info> {
+public interface SqlServer_DeptInfo extends JpaRepository<sqlserver_dept_info, Integer>, QuerydslPredicateExecutor<sqlserver_dept_info> {
 }
--- src/main/java/com/js/qa/qatools/hr/repository/sqlserver/SqlServer_Qa_Test_Hr.java
+++ src/main/java/com/js/qa/qatools/hr/repository/sqlserver/SqlServer_Qa_Test_Hr.java
@@ -1,5 +1,5 @@
 package com.js.qa.qatools.hr.repository.sqlserver;
-import com.js.qa.qatools.hr.entity.sqlserver.qa_test_hr;
+import com.js.qa.qatools.hr.entity.sqlserver.sqlserver_qa_test_hr;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -7,7 +7,7 @@ import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 import javax.transaction.Transactional;
 
-public interface SqlServer_Qa_Test_Hr extends JpaRepository<qa_test_hr, Integer>, QuerydslPredicateExecutor<qa_test_hr> {
+public interface SqlServer_Qa_Test_Hr extends JpaRepository<sqlserver_qa_test_hr, Integer>, QuerydslPredicateExecutor<sqlserver_qa_test_hr> {
 
     /**
      * AutoIncreament 초기화
