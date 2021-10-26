a11803e0b98061cee445e3776a2ce4e598356389
손정빈 <wjdqls62@naver.com>
2021-10-26 21:56:58 +0900

    #24 - MySQL Repository 인터페이스를 SQL Server로 추가

--- src/main/java/com/js/qa/qatools/hr/repository/mysql/MySQL_DeptInfo.java
+++ src/main/java/com/js/qa/qatools/hr/repository/mysql/MySQL_DeptInfo.java
@@ -1,8 +1,8 @@
 package com.js.qa.qatools.hr.repository.mysql;
 
-import com.js.qa.qatools.hr.entity.mysql.dept_info;
+import com.js.qa.qatools.hr.entity.mysql.mysql_dept_info;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface MySQL_DeptInfo extends JpaRepository<dept_info, Integer>, QuerydslPredicateExecutor<dept_info> {
+public interface MySQL_DeptInfo extends JpaRepository<mysql_dept_info, Integer>, QuerydslPredicateExecutor<mysql_dept_info> {
 }
